7926108e3285229d8dcbc080c6583018127a25f6
Mamta <mamta.mandan@gmail.com>
2019-01-19 13:11:53 -0600

    updated line in logic.js where data.csv is being pulled in

--- GlobalStockMarketAnalysis/static/js/logic.js
+++ GlobalStockMarketAnalysis/static/js/logic.js
@@ -46,7 +46,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import data from an external CSV file
-d3.csv("data.csv", function(error, myData) {
+d3.csv("../data/data.csv", function(error, myData) {
   if (error) throw error;
 
   console.log(myData);
